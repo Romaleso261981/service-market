4ffef2d61d3483c900a6bd6bb28d3faaff00782c
RomaLesyo <ladclimatservice@gmail.com>
2026-02-28 22:45:59 +0200

    Add remote image patterns to Next.js configuration: Allow images from service-market.com.ua and www.service-market.com.ua for improved asset handling.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: [],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.service-market.com.ua',
+        pathname: '/storage/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'service-market.com.ua',
+        pathname: '/storage/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
